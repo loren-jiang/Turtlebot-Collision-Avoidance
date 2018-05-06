0ec1d8582b8b3f99ee9db41318b77049cb4b5d64
Class Account <ee106b-aac@imail.EECS.Berkeley.EDU>
2018-05-05 23:35:13 -0700

    adding new files to github

--- proj_test/scripts/scan_to_cmd.py
+++ proj_test/scripts/scan_to_cmd.py
@@ -7,14 +7,14 @@ from utils import *
 import tf
 import tf.transformations as tfs
 import numpy as np 
-import scipy as scp 
+import scipy 
 import math
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan 
 from lidarClass import Lidar
 from laser_geometry import LaserProjection
 import sensor_msgs.point_cloud2 as pc2 #may want to use point cloud...not sure yet
-
+import matplotlib.pyplot as plt
 from std_msgs.msg import Empty
 from time import time
 
@@ -27,6 +27,10 @@ cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10) #auton
 # set up the odometry reset publisher
 reset_odom = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
 
+valley_mag_thresh = 0 #NEED TO SET THESE
+valley_ind_thresh = 0
+
+goal_state = np.zeros(3) #global goal state
 target_speed = np.array(.2) # m/s?
 
 
@@ -47,8 +51,32 @@ def nan_helper(y):
     return np.isnan(y), lambda z: z.nonzero()[0]
 
 #"VFH" algorithm
-def compute_cmd(state):
-    ret_twist = 0
+#data --> np array of 1/depth^2 and angle as tuple
+#state --> robot state
+def compute_cmd(state, data):
+    #valley_inds = scipy.signal.find_peaks_cwt(-data[:][0]) #need to include neg sign to be able to find "peaks"
+    #print(-data[:,0])
+    valley_inds = np.array([1, 40, 50]) #testing purposes
+    #print(valley_inds)
+    headings =  data[valley_inds][:,1] #possible headings to choose from
+    print(headings)
+    final_goal = np.zeros(3) 
+    goal_dist = np.array([0.05, 0]) #goal dist in robot frame [meters]
+    for heading in headings:
+        rot2d = rotation2d(heading) #just care about yaw
+        goal_from_world = np.dot(rot2d, goal_dist)
+        # need to double check if this is the correct transformation
+        next_goal = np.array([goal_from_world[0] + state[0], goal_from_world[1] + state[1], state[2]])
+
+        if (np.linalg.norm(goal_state[:2] - next_goal[:2]) < np.linalg.norm(goal_state[:2] - final_goal[:2])):
+            final_goal = next_goal
+
+    ret_twist = Twist()
+
+    # Need to figure out some control law to find appropriate twist
+    ret_twist.linear.x = .1
+    ret_twist.angular.z = final_goal[2]
+    print(ret_twist)
     return ret_twist
 
 def moving_average(a, n=3) :
@@ -58,7 +86,7 @@ def moving_average(a, n=3) :
 
 
 def main():
-    global cmd_vel, ROS_RATE
+    global cmd_vel, ROS_RATE, goal_state
     rospy.init_node('scan_to_cmd', anonymous=False)
     rospy.loginfo("To stop TurtleBot CTRL + C")
     rospy.on_shutdown(shutdown)
@@ -93,16 +121,20 @@ def main():
     # certainty value
     c = 1
 
+    #data visualization
+    plt.ion() ## Note this correction
+    fig=plt.figure()
+
     while not rospy.is_shutdown():
         # getting the position of the turtlebot
         robot_pos, robot_rot = listener.lookupTransform(from_frame, to_frame, listener.getLatestCommonTime(from_frame, to_frame))
-    
-        # 3x1 array, representing (x,y,theta) of robot starting state
-        robot_state = np.array([robot_pos[0], robot_pos[1], robot_rot[2]])
 
         #define goal state as in front of the current state
         goal_dist = np.array([1.5, 0])
         robot_rot = tf.transformations.euler_from_quaternion(robot_rot) #should be from -pi to pi as euler
+        # 3x1 array, representing (x,y,theta) of robot starting state
+        robot_state = np.array([robot_pos[0], robot_pos[1], robot_rot[2]])
+
         rot2d = rotation2d(robot_rot[2]) #just care about yaw
         goal_from_world = np.dot(rot2d, goal_dist)
         # need to double check if this is the correct transformation
@@ -117,36 +149,34 @@ def main():
                 
         #       mag = c*c * (a - b*d) #magnitude of pixel to turtlebot pos vector
         #       print(d)
-        if not lidar.ranges:
+
+        if not lidar.ranges or all(np.isnan(lidar.ranges)):
             continue
         else:
-            print(lidar.ranges)
             #clean the scan data and compute angle
             # clean data optimized:
             angles = np.arange(lidar.angle_min, lidar.angle_max, lidar.angle_increment)
 
             #linear interpolation nan values in laser scan
-            y = lidar.ranges
-            nans, x= nan_helper(y)
-            print(nans)
-            print(x)
-            y[nans]= np.interp(x(nans), x(~nans), y[~nans])
-
-
-            combined = zip(y, angles)
-            clean_data = filter(lambda da: not np.isnan(da[0]), combined)
-            clean_data = np.array(list(map(lambda x:(1/x[0]**2), clean_data))) #inveting distances^2
-            #clean_data = list(map(lambda x:(a - b*x[0],x[1]), clean_data)) 
-            print(robot_state)
-            print(goal_state)
-            # for (dist, ang) in clean_data:
-
-            # move_cmd = compute_cmd(robot_state)
-            twist_message = Twist()
-            twist_message.linear.x = 0.2
-            twist_message.angular.z = 0
-            cmd_vel.publish(twist_message)
+            lidar_copy = np.asarray(lidar.ranges)
+            nans, nonzero_filter = nan_helper(lidar_copy)
             
+            lidar_copy[nans] = np.interp(nonzero_filter(nans), nonzero_filter(~nans), lidar_copy[~nans])
+
+            combined = np.vstack((lidar_copy, angles)).T # shape  = (640, 2)
+            tmp = 1./(combined[:,0] *combined[:,0] )
+            combined[:,0] = tmp
+
+            move_cmd = compute_cmd(robot_state, combined) #computes twist message to cmd_vel
+            # twist_message = Twist()
+            # twist_message.linear.x = 0.2
+            # twist_message.angular.z = 0
+            cmd_vel.publish(move_cmd)
+            plt.plot(angles, 1./(lidar_copy*lidar_copy))
+            plt.draw()
+            plt.pause(0.0001) 
+        #rospy.sleep(0.05)
+        plt.clf()
         rate.sleep()    
 
 def shutdown():
